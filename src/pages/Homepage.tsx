80cb878cef3460652ef3248790b831d5d642329e
Gleb <podiumfffuuu@gmail.com>
2023-03-18 13:11:43 +0400

    feat: add cards filtration on search

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -2,13 +2,30 @@ import React, { Component } from 'react';
 
 import SearchBar from './../components/Searchbar';
 import Cards from './../components/Cards';
+import cardsData from './../data/cardsData';
 
 class HomePage extends Component {
+  state = {
+    activeCards: cardsData,
+  };
+
+  filterCards(value: string) {
+    this.setState({
+      activeCards: cardsData.filter((card) => {
+        return (
+          card.title.toLocaleLowerCase().includes(value.toLowerCase()) ||
+          card.price.toString().includes(value) ||
+          card.text.toLocaleLowerCase().includes(value.toLowerCase())
+        );
+      }),
+    });
+  }
+
   render() {
     return (
       <main>
-        <SearchBar />
-        <Cards />
+        <SearchBar filterCards={this.filterCards.bind(this)} />
+        <Cards cards={this.state.activeCards} />
       </main>
     );
   }
